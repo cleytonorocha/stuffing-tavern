175de613b8beb63b80a352553a2ae4500c87fcb8
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2024-09-04 15:18:18 -0300

    build: start orchestrator

---
+++ orchestrator/src/test/java/cleytonorocha/com/github/orchestrator/OrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package cleytonorocha.com.github.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
