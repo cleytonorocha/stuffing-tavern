6acf2680f5a971240c280664a5c83c6bb79bf0d7
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2024-09-05 16:12:34 -0300

    feat: dockerize and add connector postgres

---
+++ client/src/main/java/cleytonorocha/com/github/client/core/model/entity/Client.java
@@ -0,0 +1,8 @@
+package cleytonorocha.com.github.client.core.model.entity;
+
+// @Entity
+public class Client {
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // private Long id;
+}
